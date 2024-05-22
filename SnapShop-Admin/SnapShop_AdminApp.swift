310a4b9c5c4dc425252e95a563b8d60191ae4335
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2024-05-22 14:11:45 +0300

    Initial commit

---
+++ SnapShop-Admin/SnapShop_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapShop_AdminApp.swift
+//  SnapShop-Admin
+//
+//  Created by Hadir on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnapShop_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
