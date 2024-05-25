1a4ebaa1665f6b3de171b78e00aa3a4777e24877
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2024-05-25 12:59:12 +0300

    Install Apollo pod

---
+++ Pods/Target Support Files/Pods-SnapShop-Admin-SnapShop-AdminUITests/Pods-SnapShop-Admin-SnapShop-AdminUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapShop_Admin_SnapShop_AdminUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapShop_Admin_SnapShop_AdminUITests
+@end
---
+++ Pods/Target Support Files/Pods-SnapShop-AdminTests/Pods-SnapShop-AdminTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapShop_AdminTests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapShop_AdminTests
+@end
