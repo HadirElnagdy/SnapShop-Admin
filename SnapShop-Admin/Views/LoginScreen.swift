c83c71e51d5a3b5ed55d9b04a0f38a1d5ede72f4
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-05-31 16:37:57 +0300

    feat :Added LoginScreen  file

---
+++ SnapShop-Admin/Views/LoginScreen.swift
@@ -0,0 +1,21 @@
+//
+//  LoginScreen.swift
+//  SnapShop-Admin
+//
+//  Created by Abdullah Essam on 31/05/2024.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        VStack{
+        
+            
+        }
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
