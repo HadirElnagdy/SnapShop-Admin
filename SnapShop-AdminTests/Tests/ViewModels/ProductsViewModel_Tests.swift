bd55ef71437071c55e32764ec4363f7d1b56bdf7
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2024-06-13 21:32:34 +0300

    test: add unit tests for ProductsViewModel

--- SnapShop-AdminTests/Tests/ViewModels/ProductsViewModel_Tests.swift
+++ SnapShop-AdminTests/Tests/ViewModels/ProductsViewModel_Tests.swift
@@ -6,18 +6,92 @@
 //
 
 import XCTest
+@testable import SnapShop_Admin
 
 final class ProductsViewModel_Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     
-   
-
+    var viewModel: ProductsViewModel!
+    
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = ProductsViewModel(apiClient: MockAPIClient.self)
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
+    }
+    
+    func testGetProductsSuccess() {
+        // Given
+        let expectation = XCTestExpectation(description: "Fetch products successfully")
+        
+        // When
+        viewModel.getProducts()
+        
+        // Then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            XCTAssertFalse(self.viewModel.isLoading)
+            XCTAssertEqual(self.viewModel.productList.count, 1)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testCreateProductSuccess() {
+        // Given
+        let expectation = XCTestExpectation(description: "Create product successfully")
+        
+        // When
+        let newProduct = Product(id: 1, title: "Testing Product", description: "Testing Product description", vendor: "Testing Product vendor", productType: "Testing Product type", tags: "Testing Product tags", variants: [Variant(price: "30", inventoryQuantity: 11)], images: nil)
+        viewModel.createProduct(product: ProductRequest(product: newProduct))
+        
+        // Then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            XCTAssertEqual(self.viewModel.productList.count, 1)
+            XCTAssertEqual(self.viewModel.productList.last?.title, newProduct.title)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testUpdateProductSuccess() {
+        // Given
+        let expectation = XCTestExpectation(description: "Create product successfully")
+        
+        // When
+        let updatedProduct = Product(id: 1, title: "Testing Product", description: "Testing Product description", vendor: "Testing Product vendor", productType: "Testing Product type", tags: "Testing Product tags", variants: [Variant(price: "30", inventoryQuantity: 11)], images: nil)
+        viewModel.createProduct(product: ProductRequest(product: updatedProduct))
+        
+        // Then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            XCTAssertEqual(self.viewModel.productList.last?.id, updatedProduct.id)
+            XCTAssertEqual(self.viewModel.productList.last?.title, updatedProduct.title)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 2.0)
+    }
+    
+    func testDeleteProductSuccess() {
+        // Given
+        let expectation = XCTestExpectation(description: "Delete product successfully")
+        let productToDelete = Product(id: 1, title: "Testing Product", description: "Testing Product description", vendor: "Testing Product vendor", productType: "Testing Product type", tags: "Testing Product tags", variants: [Variant(price: "30", inventoryQuantity: 11)], images: nil)
+        viewModel.productList = [productToDelete]
+        
+        // When
+        viewModel.deleteProduct(product: productToDelete)
+        
+        // Then
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            XCTAssertTrue(self.viewModel.productList.isEmpty)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 2.0)
+    }
 }
+
