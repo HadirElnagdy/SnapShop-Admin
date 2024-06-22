cb6768f17780ecafe0e45d52d3cd38ad61a4d36a
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2024-06-22 15:58:39 +0300

    feat: add search bar to ProductsView and CollectionsView

--- SnapShop-Admin/Views/Collections/CollectionsView.swift
+++ SnapShop-Admin/Views/Collections/CollectionsView.swift
@@ -12,60 +12,75 @@ struct CollectionsView: View {
     @ObservedObject var collectionsViewModel = CollectionsViewModel()
     @State var openAddCollectionView: Bool = false
     @State var selectedCollection: Collection? = nil
+    @State private var searchText = ""
+    
+    var filteredCollections: [Collection] {
+        if searchText.isEmpty {
+            return collectionsViewModel.collections
+        } else {
+            return collectionsViewModel.collections.filter { collection in
+                collection.title!.lowercased().contains(searchText.lowercased())
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
             GeometryReader { geometry in
-                if collectionsViewModel.isLoading{
-                    LoadingLottieView()
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .onAppear{
+                VStack {
+                    SearchBar(text: $searchText)
+                        .padding(.top, 10)
+                    
+                    if collectionsViewModel.isLoading {
+                        LoadingLottieView()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .onAppear {
+                                collectionsViewModel.getCollections()
+                            }
+                    } else {
+                        ScrollView {
+                            if filteredCollections.isEmpty {
+                                VStack(alignment: .center) {
+                                    Spacer()
+                                    ContentUnavailableView(title: "No collections!", imageName: "rectangle.stack.badge.plus")
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    Spacer()
+                                }
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                            } else {
+                                CollectionsGrid(screenHeight: geometry.size.height, collections: filteredCollections, deleteCollection: { collection in
+                                    selectedCollection = collection
+                                }, viewModel: collectionsViewModel)
+                            }
+                        }
+                        .refreshable {
                             collectionsViewModel.getCollections()
                         }
-                }else{
-                    ScrollView{
-                        if collectionsViewModel.collections.isEmpty {
-                            VStack(alignment: .center) {
-                                Spacer()
-                                ContentUnavailableView(title: "No collections!", imageName: "rectangle.stack.badge.plus")
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                Spacer()
+                        .navigationBarTitle("Collections")
+                        .toolbar {
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button {
+                                    openAddCollectionView.toggle()
+                                } label: {
+                                    Image(systemName: "plus.app")
+                                        .font(.system(size: 24))
+                                }
+                                .sheet(isPresented: $openAddCollectionView) {
+                                    AddCollectionView(collectionsViewModel: collectionsViewModel)
+                                }
                             }
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                        } else {
-                            CollectionsGrid(screenHeight: geometry.size.height, collections: collectionsViewModel.collections, deleteCollection: { collection in
-                                selectedCollection = collection
-                            }, viewModel: collectionsViewModel)
+                        }
+                        .alert(item: $selectedCollection) { collection in
+                            Alert(
+                                title: Text("Confirm Deletion"),
+                                message: Text("Are you sure you want to delete this collection?"),
+                                primaryButton: .destructive(Text("Delete")) {
+                                    collectionsViewModel.deleteCollection(collection: collection)
+                                },
+                                secondaryButton: .cancel(Text("Cancel"))
+                            )
                         }
                     }
-                    .refreshable {
-                        collectionsViewModel.getCollections()
-                    }
-                    .navigationBarTitle("Collections")
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            Button{
-                                openAddCollectionView.toggle()
-                            }label: {
-                                Image(systemName: "plus.app")
-                                    .font(.system(size: 24))
-                            }
-                            .sheet(isPresented: $openAddCollectionView){
-                                AddCollectionView(collectionsViewModel: collectionsViewModel)
-                            }
-                            
-                        }
-                    }.alert(item: $selectedCollection) { collection in
-                        Alert(
-                            title: Text("Confirm Deletion"),
-                            message: Text("Are you sure you want to delete this collection?"),
-                            primaryButton: .destructive(Text("Delete")) {
-                                collectionsViewModel.deleteCollection(collection: collection)
-                            },
-                            secondaryButton: .cancel(Text("Cancel"))
-                        )
-                    }
-                   
                 }
             }
         }
@@ -75,5 +90,3 @@ struct CollectionsView: View {
 #Preview {
     CollectionsView()
 }
-
-
--- SnapShop-Admin/Views/Products/ProductsView.swift
+++ SnapShop-Admin/Views/Products/ProductsView.swift
@@ -13,64 +13,79 @@ struct ProductsView: View {
     @State var openAddProductView: Bool = false
     @ObservedObject var productsViewModel = ProductsViewModel()
     @State var selectedProduct: Product? = nil
+    @State private var searchText = ""
+    
+    var filteredProducts: [Product] {
+        if searchText.isEmpty {
+            return productsViewModel.productList
+        } else {
+            return productsViewModel.productList.filter { product in
+                product.title!.lowercased().contains(searchText.lowercased())
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
             GeometryReader { geometry in
-                ScrollView {
-                    if productsViewModel.isLoading{
-                        LoadingLottieView()
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    }else{
-                        if productsViewModel.productList.isEmpty {
-                            VStack(alignment: .center) {
-                                Spacer()
-                                ContentUnavailableView(title: "No products!", imageName: "cart.badge.plus")
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                Spacer()
+                VStack {
+                    SearchBar(text: $searchText)
+                                            .padding(.top, 10)
+                    ScrollView {
+                        if productsViewModel.isLoading{
+                            LoadingLottieView()
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }else{
+                            if filteredProducts.isEmpty {
+                                VStack(alignment: .center) {
+                                    Spacer()
+                                    ContentUnavailableView(title: "No products!", imageName: "cart.badge.plus")
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    Spacer()
+                                }
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                            } else {
+                                ProductsGrid(screenHeight: geometry.size.height, productsList: filteredProducts, deleteProduct: { product in
+                                    selectedProduct = product
+                                })
                             }
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                        } else {
-                            ProductsGrid(screenHeight: geometry.size.height, productsList: productsViewModel.productList, deleteProduct: { product in
-                                selectedProduct = product
-                            })
+                        }
+                        
+                    }
+                    .refreshable {
+                        productsViewModel.getProducts()
+                    }
+                    .navigationBarTitle("Products")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button {
+                                openAddProductView.toggle()
+                            } label: {
+                                Image(systemName: "plus.app")
+                                    .font(.system(size: 24))
+                            }
+                            .sheet(isPresented: $openAddProductView){
+                                AddProductView(productsViewModel: productsViewModel)
+                            }
                         }
                     }
-                    
-                }
-                .refreshable {
-                    productsViewModel.getProducts()
-                }
-                .navigationBarTitle("Products")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            openAddProductView.toggle()
-                        } label: {
-                            Image(systemName: "plus.app")
-                                .font(.system(size: 24))
-                        }
-                        .sheet(isPresented: $openAddProductView){
-                            AddProductView(productsViewModel: productsViewModel)
-                        }
+                    .alert(item: $selectedProduct) { product in
+                        Alert(
+                            title: Text("Confirm Deletion"),
+                            message: Text("Are you sure you want to delete this product?"),
+                            primaryButton: .destructive(Text("Delete")) {
+                                productsViewModel.deleteProduct(product: product)
+                            },
+                            secondaryButton: .cancel(Text("Cancel"))
+                        )
                     }
+                    .onAppear{
+                        productsViewModel.getProducts()
                 }
-                .alert(item: $selectedProduct) { product in
-                    Alert(
-                        title: Text("Confirm Deletion"),
-                        message: Text("Are you sure you want to delete this product?"),
-                        primaryButton: .destructive(Text("Delete")) {
-                            productsViewModel.deleteProduct(product: product)
-                        },
-                        secondaryButton: .cancel(Text("Cancel"))
-                    )
-                }
-                .onAppear{
-                    productsViewModel.getProducts()
                 }
             }
         }
-       
+        
     }
 }
 
